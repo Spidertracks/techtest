4da94322df240735014cec5f9e22ed49e693dc4d
Gustavo Iglesias <gustavo.iglesias@spidertracks.co.nz>
2023-10-13 17:08:25 +1300

    Setting up tech challange.

---
+++ src/exercise.ts
@@ -0,0 +1,22 @@
+/**
+ * Spidertracks Limited
+ * All rights reserved.
+ * 
+ * Exercise:
+ * =========
+ * 
+ * Write a function that checks if a given word or phrase is a palindrome (reads the same backward as forward), 
+ * ignoring spaces and punctuation.
+ * 
+ * example of words or phrases are:
+ * - pop
+ * - step on no pets
+ * - rotor
+ * - noon
+ * - A man, a plan, a canal, Panama!
+ * 
+ */
+
+module.exports = (palindromes: string[]) => {
+  console.log('// TODO: Implement me!')
+}
---
+++ tests/index.spec.ts
@@ -0,0 +1,5 @@
+describe('Default test', () => {
+    it('test 1.1', () => {
+        expect(1).toBe(1)
+    })
+})
