f09ab4bc24d932ef0ac48b8dba335c4262a278a8
Gustavo Iglesias <gustavo.iglesias@spidertracks.co.nz>
2023-10-13 17:23:56 +1300

    Removing list of words to test.

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,9 @@
  */
 const exercise = require("./exercise");
 
-const palindromesToCheck = [
-    'pop',
-    'step on no pets',
-    'rotor',
-    'noon',
-    'A man, a plan, a canal, Panama!'
-]
-
-const nonPalindromesToCheck = [
-    'hello',
-    'world',
-    'spidertracks',
-    'This is not a palindrome'
-]
-
 function main() {
-  console.log('Checking palindromes')
-  exercise(palindromesToCheck);
-
-  console.log('Checking non-palindromes')
-  exercise(nonPalindromesToCheck);
+  console.log('Running exercise...')
+  exercise("");
 }
 
 main();
